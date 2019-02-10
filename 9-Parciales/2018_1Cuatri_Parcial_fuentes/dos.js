f75e6cd4960ea42f49e47be68358b2d9243bd3fe
Matiteh <matias.molinada@hotmail.com>
2019-02-10 17:59:27 -0300

    3 primeros ejercicios de parcial completados

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,11 @@
 function mostrar()
 {
+	var nombr;
+	var locali;
+
+	nombr= elNombre.value;
+	locali= laLocalidad.value;
+
+	alert("usted es "+nombr+" y vive en la localidad de "+locali);
   
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+	var precio;
+	var desc;
+	var prefinal;
 
+	precio= prompt("ingrese el precio");
+	desc= prompt("ingrese % de descuento");
+
+	prefinal= (precio*desc)/100;
+
+	elPrecioFinal.value= prefinal;
 }
