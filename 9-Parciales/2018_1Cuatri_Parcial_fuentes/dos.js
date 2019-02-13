19319dda1bbeee1bc3c71c53837941bb4f435a79
alumno <alumno>
2019-02-13 09:20:57 -0300

    2do Ejercicio dictado en clase completo

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,11 +1,15 @@
 function mostrar()
 {
-	var nombr;
-	var locali;
+	var precioprod;
+	var recargo;
+	var preciofinal;
 
-	nombr= elNombre.value;
-	locali= laLocalidad.value;
+	precioprod=prompt("Ingrese el precio del producto");
+	precioprod=parseInt(precioprod);
+	recargo=(precioprod*10)/100;
+	preciofinal=precioprod+recargo;
 
-	alert("usted es "+nombr+" y vive en la localidad de "+locali);
+	alert("El precio del producto en efectivo es: $"+precioprod+", con tarjeta tiene un recargo del 10% que seria $"+recargo+" de recargo, costandole final: $"+preciofinal);
+
   
 }
