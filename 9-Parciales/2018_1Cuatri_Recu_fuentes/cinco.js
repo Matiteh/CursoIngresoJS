d11cb16c0f89e6e1fc1294c18b4fba376082f8c7
Matiteh <matias.molinada@hotmail.com>
2019-02-20 12:17:51 -0300

    Ejercicios planteados en clase completados

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,62 @@
 function mostrar()
 {
+	/*cada habitacion de un hotel tiene un precio, hay promociones segun el tipo de pago, si es con tarjeta visa 10%
+	, si es por paypal 15%, si es por mercado pago 10%, en efectivo 20%, cualquier otro medio 5%.
+	si compraste el paquete "todo incluido" y pagas con paypal, se te suma un 10% al descuento. si pagas en efectivo,
+	tenes varias opciones, el paquete "solo desayunos" te suma un 10% el descuento, si el paquete es "todo incluido" te suna un 15%
+	y para el resto de los paquetes no tiene descuento adicional*/
+
+	var preciohabitacion;
+	var paquete;
+	var metododepago;
+	var desc1;
+	var descfinal;
+	var preciofinal;
+	
+
+
+	preciohabitacion=prompt("Ingrese el precio de la habitacion");
+	paquete=prompt("Ingrese el paquete que eligiò");
+	preciohabitacion=parseInt(preciohabitacion);
+	metododepago=prompt("Ingrese el metodo de pago");
+
+	switch (metododepago)
+	{
+		case "visa":
+		case "mercado pago":
+			desc1=10;
+			break;
+		case "paypal":
+			switch (paquete)
+			{
+				case "todo incluido":
+					desc1=25;
+					break;
+				default:
+					desc1=15;
+					break;
+			}
+		case "efectivo":
+			switch(paquete)
+			{
+				case "solo desayunos":
+					desc1=30;
+					break;
+				case "todo incluido":
+					desc1=35;
+					break;
+				case "otro paquete":
+					desc1=20;
+					break;
+			}
+		case "otro medio":
+			desc1=5;
+			break;
+	}
+
+	descfinal=(preciohabitacion*desc1)/100;
+	preciofinal=preciohabitacion-descfinal;
+
+	alert(preciofinal);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,83 @@
 function mostrar()
 {
+	//ingresa 4 importes, mostrar el mayor, mostrar cuanto es el total y si supera 100 un desc 10% , si supera los 50 un desc 5% de desc, si es menos de 50 recargo 15%
+
+	var import1;
+	var import2;
+	var import3;
+	var import4;
+	var totalimport;
+	var desc10;
+	var desc5;
+	var recargo15;
+	var valorfinal;
+	var msjdescorec;
+	var importemayor;
+
+	import1=prompt("Ingrese el primer importe");
+	import1=parseInt(import1);
+	import2=prompt("Ingrese el segundo importe");
+	import2=parseInt(import2);
+	import3=prompt("Ingrese el tercer importe");
+	import3=parseInt(import3);
+	import4=prompt("Ingrese el cuarto importe");
+	import4=parseInt(import4);
+
+	totalimport=import1+import2+import3+import4;
+
+	//Buscamos el mayor importe
+	if (import1>import2 && import1>import3 && import1>import4)
+	{
+		importemayor=import1;
+	}
+	else
+	{
+		if (import2>import1 && import2>import3 && import2>import4)
+		{
+			importemayor=import2;
+		}
+		else
+		{
+			if (import3>import1 && import3>import2 && import3>import4)
+			{
+				importemayor=import3;
+			}
+			else
+			{
+				importemayor=import4;
+			}
+		} 	
+	} 
+	
+	
+
+	//Realizamos las operaciones de descuento y recargo
+	if (totalimport>100)
+	{
+		desc10=(totalimport*10)/100;
+		valorfinal=totalimport-desc10;
+		msjdescorec=("con un descuento del 10%")
+	}
+	else
+	{
+		if (totalimport<100 && totalimport>50)
+		{
+			desc5=(totalimport*5)/100;
+			valorfinal=totalimport-desc5;
+			msjdescorec=("con un descuento del 5%")
+		}
+		else
+		{
+			if (totalimport<50)
+			{
+				recargo15=(totalimport*15)/100;
+				valorfinal=totalimport+recargo15;
+				msjdescorec=("con un recargo del 15%")
+			}
+		}
+	}
+	
+		
+	alert("El mayor importe es de: $"+importemayor+", el total de los importes es: $"+totalimport+" y el total final de la compra "+msjdescorec+" es de: $"+valorfinal);
 
 }
